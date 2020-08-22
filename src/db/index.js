de8496b9af37576fdfe6311cf20b9d8e463d71c0
EMIRATES\S797392 <mohamedyousuf.rifay@dnata.com>
2020-08-22 07:56:01 +0400

    Add: test data update

--- src/db/index.js
+++ src/db/index.js
@@ -2,13 +2,13 @@ export const history = [
     {
         'playerOne': 'Yousuf',
         'playerTwo': 'Fahma',
-        'date' : '5-19-2022',
+        'date' : '05-19-2022',
         'winner': 'Yousuf'
     },
     {
         'playerOne': 'Rifay',
         'playerTwo': 'Ayash',
-        'date' : '5-19-2022',
+        'date' : '05-19-2022',
         'winner': 'Ayash'
     }
 ];
